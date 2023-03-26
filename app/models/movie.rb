d235fc71a03e54fc1f07f3a01b6c0a9a0d01e663
UAN MURPHY <g00385338@atu.ie>
2023-03-26 18:10:10 +0000

    Heroku Test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+  
+  def self.all_ratings()
+    return ['G', 'PG', 'PG-13', 'R']
+  end
+  
+  def self.with_ratings(ratings_list)
+    return movies
+  end
+
 end
